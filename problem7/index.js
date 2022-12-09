2e1fe30188b9ab478a01f0f26a63cfa85467fb17
vlecommandeur <valentin.lecommandeur@gmail.com>
2022-12-09 13:35:50 +0100

    ok now the code is embarrassing, but i have a job

--- problem7/index.js
+++ problem7/index.js
@@ -92,4 +92,111 @@ const calculateDirectoriesSizesSum = () => {
   return directoriesSizesSum;
 };
 
+const calculateDeletedDirectorySize = () => {
+  const tree = new Tree();
+  tree.root = new Node({
+    name: "/",
+    value: 0,
+    id: uuidv4(),
+    type: "directory",
+  });
+
+  let currentNode = tree.root;
+
+  input.forEach((command) => {
+    const commandArr = command.split(" ");
+
+    if (commandArr[0] === "dir") {
+      currentNode.add({
+        name: commandArr[1],
+        value: 0,
+        id: uuidv4(),
+        type: "directory",
+      });
+    }
+
+    if (commandArr[0] === "cd") {
+      if (commandArr[1] !== "..") {
+        currentNode = currentNode.children.find(
+          (child) =>
+            child.data.name === commandArr[1] && child.data.type === "directory"
+        );
+      }
+      if (commandArr[1] === "..") {
+        tree.traverseBF((node) => {
+          if (
+            node.children.find((child) => {
+              return (
+                child.data.id === currentNode.data.id &&
+                child.data.type === "directory"
+              );
+            })
+          ) {
+            currentNode = node;
+          }
+        });
+      }
+    }
+
+    if (!instructions.includes(commandArr[0])) {
+      if (
+        currentNode.children.some(
+          (child) =>
+            child.data.name === commandArr[1] && child.data.type === "file"
+        )
+      ) {
+        currentNode.children.find(
+          (child) =>
+            child.data.name === commandArr[1] && child.data.type === "file"
+        ).data.value += commandArr[0];
+      } else {
+        currentNode.add({
+          name: commandArr[1],
+          value: commandArr[0],
+          id: uuidv4(),
+          type: "file",
+        });
+      }
+    }
+  });
+
+  let rootDirectorySize = 0;
+
+  tree.traverseBF((node) => {
+    if (node.data.type === "directory") {
+      let sum = 0;
+
+      const test = new Tree();
+      test.root = node;
+
+      test.traverseBF((nodeTest) => {
+        if (nodeTest.data.type === "file") sum += parseInt(nodeTest.data.value);
+      });
+
+      if (sum > rootDirectorySize) rootDirectorySize = sum;
+    }
+  });
+
+  let remainingSpace = 30000000 - (70000000 - rootDirectorySize);
+  let result = 9999999999999999999999999999999;
+
+  tree.traverseBF((node) => {
+    if (node.data.type === "directory") {
+      let sum = 0;
+
+      const test = new Tree();
+      test.root = node;
+
+      test.traverseBF((nodeTest) => {
+        if (nodeTest.data.type === "file") sum += parseInt(nodeTest.data.value);
+      });
+
+      if (sum > remainingSpace && sum < result) result = sum;
+    }
+  });
+
+  return result;
+};
+
 console.log("Directories sizes sum:", calculateDirectoriesSizesSum());
+console.log("Deleted directory size:", calculateDeletedDirectorySize());
